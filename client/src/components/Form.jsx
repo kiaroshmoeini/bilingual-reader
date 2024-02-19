3e9eceaa3006aeb1685d7b30293498f12c2e9562
Kiarosh Moeini <kmoeini@gmail.com>
2024-02-19 16:40:32 +0900

    added inputs

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <>
+        <h1>Bilingual Reader</h1>
+        <form>
+            <input type = "text" className="language1" />
+            <input type = "text" className="language2" />
+
+        </form>
+        </>
+    );
+};
+
+export default Form;
